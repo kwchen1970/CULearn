445243c1d85d7262c863108190b281fe446fe76c
Kathy Chen <kc994@cornell.edu>
2024-04-28 01:47:14 -0400

    finished student model

--- src/app.py
+++ src/app.py
@@ -1,5 +1,70 @@
 from db import db
 import json
 from flask import Flask,request
-from db import Course, Assignment,User
+from db import Student
 
+app = Flask(__name__)
+db_filename = "tutor.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+# generalized response formats
+def success_response(data, code=200):
+    return json.dumps(data), code
+
+def failure_response(message, code=404):
+    return json.dumps(message), code
+
+@app.route("/api/students/")
+def get_allstudents():
+    """
+    get all students
+    """
+    return success_response({"courses": [t.serialize() for t in Student.query.all()]})
+
+app.route("/api/students/<int:student_id>/")
+def get_spec_student(student_id):
+    """
+    get specific students
+    """
+    student = Student.query.filter_by(id = student_id).first()
+    if student is None:
+        return failure_response("student not found")
+    return success_response(student.serialize())
+
+app.route("/api/students/<int:student_id>/", methods = ["DELETE"])
+def delete_student(student_id):
+    """
+    delete a student
+    """
+    student = Student.query.filter_by(id = student_id).first()
+    if student is None:
+        return failure_response("student not found")
+    db.session.delete(student)
+    db.session.commit()
+    return success_response(student.serialize())
+
+app.route("/api/students/", methods = ["POST"])
+def create_student():
+    """
+    create a student
+    """
+    body = json.loads(request.data)
+    new_student = Student(
+        name = body.get("name"),
+        username = body.get("username"),
+        password = body.get("password"),
+        profile_img = body.get("profile_img"),
+        bio = body.get("bio",""),
+        budget = body.get("budget"),
+        subjects = body.get("subjects")
+    )
+    db.session.add(new_student)
+    db.session.commit()
+    return success_response(new_student.serialize(),201)
--- src/db.py
+++ src/db.py
@@ -1,13 +1,49 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-app = Flask(__name__)
-db_filename = "tutor.db"
+class Student(db.Model):
+    __tablename__ = "student"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    name = db.Column(db.String, nullable = False)
+    username = db.Column(db.String, nullable = False)
+    password = db.Column(db.String, nullable = False)
+    profile_img = db.Column(db.String, nullable = False)
+    bio = db.Column(db.String, nullable = False)
+    budget = db.Column(db.Integer, nullable = False)
+    subjects = db.Column(db.String, nullable = False) #make this a string list
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_ECHO"] = True
-
-db.init_app(app)
-with app.app_context():
-    db.create_all()
+    def __init__(self,**kwargs):
+        """
+        Initialize a Student object
+        """
+        self.name = kwargs.get("name","")
+        self.username = kwargs.get("username","")
+        self.password = kwargs.get("password","")
+        self.profile_img = kwargs.get("profile_img","")
+        self.bio = kwargs.get("bio","")
+        self.budget = kwargs.get("budget","")
+        self.subjects = kwargs.get("subjects","")
+    
+    def serialize(self):
+        return{
+            "id":self.id,
+            "name": self.name,
+            "username":self.username,
+            "profile_img":self.profile_img,
+            "bio":self.bio,
+            "budget":self.budget,
+            "subjects":self.subjects
+        }
+    def simple_serialize(self):
+        """
+        Serialize a student object.
+        """
+        return{
+            "id":self.id,
+            "name": self.name,
+            "username":self.username,
+            "profile_img":self.profile_img,
+            "bio":self.bio,
+            "budget":self.budget,
+            "subjects":self.subjects
+        }
